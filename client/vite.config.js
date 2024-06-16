f6d9ab361f607bda8264d3bed8cff629b41a1e34
Ananya Verma <ananyaa2311@gmail.com>
2024-06-16 17:14:53 +0530

    Add deploy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/visualize-data-visualisation-dashboard",
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'https://visualize-data-visualisation-dashboard.onrender.com',
-        secure: false,
+        changeOrigin: true,
+        // secure: false,
       },
     },
   },
